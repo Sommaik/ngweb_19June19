a2b2da60152e11db92427c5d9c1c48b35ec52bfe
SOMMAI KRANGPANICH <sommai.k@gmail.com>
2019-06-20 14:14:45 +0700

    register

--- src/app/page/register/register.component.ts
+++ src/app/page/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { RegisterService } from '../../service/register.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  constructor(
+    private fb: FormBuilder,
+    private registerService: RegisterService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  registerForm = this.fb.group({
+    userId: ['', [Validators.required]],
+    name: ['', [Validators.required]],
+    password: ['', [Validators.required]]
+  });
 
-  ngOnInit() {
+  ngOnInit() {}
+  onFormSubmit() {
+    if (this.registerForm.valid) {
+      this.registerService.regis(this.registerForm.value).subscribe(result => {
+        this.router.navigate(['login']);
+      });
+    } else {
+      alert('Invalid register form');
+    }
   }
-
 }
