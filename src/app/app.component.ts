0dcbdfdab1a6dd77418c884cd669b6b74cc7fa5c
SOMMAI KRANGPANICH <sommai.k@gmail.com>
2019-06-21 15:58:11 +0700

    support lang

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  lang = 'EN';
   title = 'ngweb';
+
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
 
   onGotoClick(value1: string) {
     alert(value1);
+  }
+
+  switchLang() {
+    if (this.lang === 'EN') {
+      this.translate.use('th');
+      this.lang = 'TH';
+    } else {
+      this.translate.use('en');
+      this.lang = 'EN';
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,19 @@ import { FormGroupComponent } from './page/form-group/form-group.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HighlightDirective } from './highlight.directive';
 import { TrimCreditCardPipe } from './shared/trim-credit-card.pipe';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { PostListComponent } from './page/post-list/post-list.component';
 import { PublicZoneComponent } from './page/public-zone/public-zone.component';
 import { PrivateZoneComponent } from './page/private-zone/private-zone.component';
 import { LoginComponent } from './page/login/login.component';
 import { RegisterComponent } from './page/register/register.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -36,7 +43,14 @@ import { RegisterComponent } from './page/register/register.component';
     FormsModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
